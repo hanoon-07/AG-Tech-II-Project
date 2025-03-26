7340ebc84467d3f9843244c72e26f106de22c322
Fahad <fahad.shahid@hashlogics.com>
2025-03-26 15:56:38 +0500

    Updated the New code some issue of styling but I fix that.

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export const theme = {
+    extend: {
+        colors: {
+            primary: "#2563EB", // Custom blue color
+            
+        },
+    },
+};
+export const plugins = [];
+  
