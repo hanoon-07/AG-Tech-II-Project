9692072fc6991bc7087702f5389727b7b30902bf
Ahmad Wali Hemat <ahmadwalihemat67@gmail.com>
2025-04-09 22:32:40 +0430

    add colors to global css

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,17 @@
 // tailwind.config.js
 export const theme = {
-    extend: {
-        colors: {
-            primary: "#2563EB", // Custom blue color
-            
-        },
+  extend: {
+    colors: {
+      primary: "#2563EB", // Custom blue color
+      secondary: "#FFFFFF", // Light background or card background
+      lightRed: "#EB664E",
+      darkRed: "#A63446",
+      shadows50Percent: "#1A1A1A80",
+      shadows70Percent: "#3D3E3FB2",
+      darkBlue: "#031928",
+      dark: "#1A1A1A",
+      gray: "#00000080",
     },
+  },
 };
 export const plugins = [];
-  
